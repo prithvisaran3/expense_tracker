28f95aeaadf53a2f66dc79916fb1549fde403791
Prithvi Saran S <prithvisaran.s@gmail.com>
2023-03-01 13:07:39 +0530

    Login screen without swipe

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -1,71 +1,141 @@
+import 'dart:math';
+import 'dart:ui';
+import 'package:expense_tracker/utils/constant.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
-class login extends StatefulWidget {
-  const login({Key? key}) : super(key: key);
-
-  @override
-  State<login> createState() => _loginState();
-}
-
-class _loginState extends State<login> {
-  Widget topWidget(double screenWidth) {
-    return Transform.rotate(
-      angle: -35 * math.pi / 180,
-      child: Container(
-        width: 1.2 * screenWidth,
-        height: 1.2 * screenWidth,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(150),
-          gradient: LinearGradient(
-            begin: Alignment(-0.2, -0.8),
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0x007CBFCF),
-              Color(0xB316BFC4),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-  Widget bottomWidget(double screenWidth)
-  {
-    return Container(
-      height: 1.5*screenWidth,
-      width: 1.5*screenWidth,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        gradient: LinearGradient(
-          begin: Alignment(0.6,-1.1),
-          end:Alignment(0.7,0.8),
-          colors: [
-            Color(0xDB4BE8CC),
-            Color(0x005CDBCF),
-          ]
-
-        )
-      ),
-    );
-  }
+class login extends StatelessWidget {
+  login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: -260,
-            left: -40,
-            child: topWidget(screenSize.width),
-          ),
-          Positioned(
-            bottom: -320,
-              left: -30,
-              child: bottomWidget(screenSize.width)
-          ),
-        ],
+      body: SafeArea(
+        child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 80),
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          text: "Hey,  \nLogin",
+                          children: [
+                            TextSpan(
+                              style: TextStyle(
+                                color: kPrimaryColor,
+                              ),
+                              text: " Now.",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      prefixIcon: Icon(Icons.mail, color: kPrimaryColor),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      hintText: "Enter your email ID",
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      prefixIcon:
+                          Icon(Icons.lock_clock_outlined, color: kPrimaryColor),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      hintText: "Enter your password",
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return login();
+                      }));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      elevation: 1,
+                      primary: kPrimaryColor,
+                      textStyle: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account? ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return login();
+                          }));
+                        },
+                        child: Text(
+                          "Signup",
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const kBackgroundcolor = Colors.black;
-const kPrimaryColor= Color(0xFF16FF00);
+const kPrimaryColor= Color(0xFF39FF14);
 const kSecondaryColor=Color(0xFF265DAB);
