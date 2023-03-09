1a1cd98136d266c64455470c8335017dae5b804d
SriSenthur11 <srisenthur1105@gmail.com>
2023-03-09 11:48:28 +0530

    Signup Updated

--- lib/app/ui/screens/Signup.dart
+++ lib/app/ui/screens/Signup.dart
@@ -1,7 +1,9 @@
 
 import 'package:expense_tracker/app/ui/screens/loginscreen.dart';
 import 'package:expense_tracker/app/ui/themes/app_colors.dart';
+import 'package:expense_tracker/app/ui/widget/common_button.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/app/ui/widget/common_textform_field.dart';
 
 class esignup extends StatelessWidget {
   const esignup({Key? key}) : super(key: key);
@@ -45,117 +47,79 @@ class esignup extends StatelessWidget {
                     ],
                   ),
                   SizedBox(height: 8),
-                  TextFormField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      prefixIcon: Align(
-                        widthFactor: 1.0,
-                        heightFactor: 1.0,
-                        child: Icon(
-                          Icons.person,
-                          color: ePrimaryColor,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:  BorderSide(color: ewhite),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      hintText: "Name",
-                      hintStyle: TextStyle(color: ewhite),
-                    ),
-                  ),
+                  CommonTextFormField(
+                      prefixIcon: Icon(Icons.person,color: ePrimaryColor),
+                      hintText: "Enter your Name",
+                      // controller: AuthController.to.lEmail,
+                      validator: (data) {
+                        if (data!.isEmpty || data == "") {
+                          return "Name field required";
+                        }
+                        return null;
+                      }),
+                  // TextFormField(
+                  //   keyboardType: TextInputType.name,
+                  //   decoration: InputDecoration(
+                  //     prefixIcon: Align(
+                  //       widthFactor: 1.0,
+                  //       heightFactor: 1.0,
+                  //       child: Icon(
+                  //         Icons.person,
+                  //         color: ePrimaryColor,
+                  //       ),
+                  //     ),
+                  //     enabledBorder: OutlineInputBorder(
+                  //       borderSide:  BorderSide(color: ewhite),
+                  //       borderRadius: BorderRadius.circular(8),
+                  //     ),
+                  //     hintText: "Name",
+                  //     hintStyle: TextStyle(color: ewhite),
+                  //   ),
+                  // ),
                   SizedBox(height: 20),
-                  TextFormField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                      prefixIcon: Align(
-                        widthFactor: 1.0,
-                        heightFactor: 1.0,
-                        child: Icon(
-                          Icons.phone_android,
-                          color: ePrimaryColor,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: ewhite),
-                          borderRadius: BorderRadius.circular(8)),
-                      hintText: "Phone Number",
-                      hintStyle: TextStyle(color: ewhite),
-                    ),
-                  ),
+                  CommonTextFormField(
+                      prefixIcon: Icon(Icons.phone,color: ePrimaryColor),
+                      hintText: "Enter your Phone Number",
+                      // controller: AuthController.to.lEmail,
+                      validator: (data) {
+                        if (data!.isEmpty || data == "") {
+                          return "Phone Number field required";
+                        }
+                        return null;
+                      }),
                   SizedBox(height: 20),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      prefixIcon: Align(
-                        widthFactor: 1.0,
-                        heightFactor: 1.0,
-                        child: Icon(
-                          Icons.email_rounded,
-                          color: ePrimaryColor,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide:  BorderSide(color: ewhite),
-                          borderRadius: BorderRadius.circular(8)),
-                      hintText: "Email",
-                      hintStyle: TextStyle(color: ewhite),
-                    ),
-                  ),
+                  CommonTextFormField(
+                      prefixIcon: Icon(Icons.email_rounded,color: ePrimaryColor),
+                      hintText: "Enter your Email",
+                      // controller: AuthController.to.lEmail,
+                      validator: (data) {
+                        if (data!.isEmpty || data == "") {
+                          return "Email field required";
+                        }
+                        return null;
+                      }),
                   SizedBox(height: 20),
-                  TextFormField(
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      prefixIcon: Align(
-                        widthFactor: 1.0,
-                        heightFactor: 1.0,
-                        child: Icon(
-                          Icons.key,
-                          color: ePrimaryColor,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide:  BorderSide(color: ewhite),
-                          borderRadius: BorderRadius.circular(8)),
-                      hintText: "Password",
-                      hintStyle: TextStyle(color: ewhite),
-                    ),
+                  CommonTextFormField(
+                      prefixIcon: Icon(Icons.key,color: ePrimaryColor),
+                      hintText: "Enter your Password",
+                      // controller: AuthController.to.lEmail,
+                      validator: (data) {
+                        if (data!.isEmpty || data == "") {
+                          return "Password field required";
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 40),
+                  CommonButton(
+                      text: "SignUp",
+                      onPressed: (){
+                        Navigator.push(context,
+                        MaterialPageRoute(builder: (context){
+                          return esignup();
+                        }));
+                      }
                   ),
                   SizedBox(height: 40),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return esignup();
-                          },
-                        ),
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 20, right: 20, top: 10, bottom: 10),
-                      child: Container(
-                          child: Text(
-                        "Sign Up",
-                        style: TextStyle(color: eblack),
-                      )),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        elevation: 1,
-                        primary: ePrimaryColor,
-                        textStyle: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        )),
-                  ),
-
-                  //SizedBox(height: 40),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
